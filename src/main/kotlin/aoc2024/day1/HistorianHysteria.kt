8e71170fd797c3122e6dbf4bf15e94c446abf8cc
Tim Mortimer <tim@kiteframe.co.uk>
2024-12-01 14:35:13 +0000

    Complete day 1 part 2

--- src/main/kotlin/aoc2024/day1/HistorianHysteria.kt
+++ src/main/kotlin/aoc2024/day1/HistorianHysteria.kt
@@ -4,24 +4,33 @@ import kotlin.math.abs
 
 fun main() {
     part1()
+    part2()
 }
 
 fun part1() {
     println(calculateDistanceBetweenInputLists("/day1/input.txt"))
 }
 
+fun part2() {
+    println(calculateSimilarityScoreOfInputLists("/day1/input.txt"))
+}
+
 fun calculateDistanceBetweenInputLists(uri: String) =
     readLists(uri)?.let { (firstList, secondList) -> calculateTotalDistanceBetween(firstList, secondList) }
 
-private fun readLists(uri: String): Pair<List<Int>, List<Int>>? {
-    return { }.javaClass.getResource(uri)
-        ?.readText()
-        ?.lines()
-        ?.map { it.split("   ") }
-        ?.filter { it.size == 2 }
-        ?.map { Pair(it[0].toInt(), it[1].toInt()) }
-        ?.unzip()
-}
+fun calculateSimilarityScoreOfInputLists(uri: String) =
+    readLists(uri)?.let { (firstList, secondList) -> calculateSimilarityScore(firstList, secondList) }
+
+private fun readLists(uri: String): Pair<List<Int>, List<Int>>? = { }.javaClass.getResource(uri)
+    ?.readText()
+    ?.lines()
+    ?.map { it.split("   ") }
+    ?.filter { it.size == 2 }
+    ?.map { Pair(it[0].toInt(), it[1].toInt()) }
+    ?.unzip()
 
 fun calculateTotalDistanceBetween(firstList: List<Int>, secondList: List<Int>) =
     firstList.sorted().zip(secondList.sorted()).sumOf { abs(it.second - it.first) }
+
+fun calculateSimilarityScore(firstList: List<Int>, secondList: List<Int>) =
+    firstList.sumOf { firstListEntry -> firstListEntry * secondList.count { firstListEntry == it } }
---
+++ src/test/kotlin/aoc2024/day1/SimilarityScoreTest.kt
@@ -0,0 +1,26 @@
+package aoc2024.day1
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class SimilarityScoreTest {
+    @Test
+    fun `two empty lists give 0`() {
+        assertEquals(0, calculateSimilarityScore(emptyList<Int>(), emptyList<Int>()))
+    }
+
+    @Test
+    fun `two identical lists give the value in the first list`() {
+        assertEquals(2, calculateSimilarityScore(listOf(2), listOf(2)))
+    }
+
+    @Test
+    fun `two lists with one repeat in second list, one single and one missing`() {
+        assertEquals(5, calculateSimilarityScore(listOf(2, 1, 5), listOf(2, 2, 1)))
+    }
+
+    @Test
+    fun `example input`() {
+        assertEquals(31, calculateSimilarityScoreOfInputLists("/day1/part2/example.txt"))
+    }
+}
